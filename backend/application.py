4184569b65b579237eb2a0a566c66b43a91c0338
mutotu <muttalip.aydemir@gmail.com>
2022-02-02 19:55:17 -0800

    transfering from enterprice

--- backend/application.py
+++ backend/application.py
@@ -4,7 +4,9 @@ from flask_cors import CORS
 import sqlalchemy
 app = Flask(__name__)
 CORS(app)
-
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+import jwt
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 import models
 models.db.init_app(app)
@@ -12,6 +14,87 @@ models.db.init_app(app)
 def root():
   return 'ok'
 app.route('/', methods=["GET"])(root)
+
+@app.route('/users', methods=["POST"])
+def create_user():
+  # notice that in models the paswotd is not there in to_json func because of 
+  hashed_pw = bcrypt.generate_password_hash(request.json["password"]).decode("utf-8")
+  try:
+      user = models.User(
+        email=request.json["email"],
+        # password=request.json["password"]
+        password=hashed_pw
+      )
+      models.db.session.add(user)
+      models.db.session.commit()
+      encrypted_id = jwt.encode({"user_id":user.id}, os.environ.get("JWT_SECRET"), algorithm="HS256")
+      return {"user":user.to_json(),
+              "user_id":encrypted_id}
+  except sqlalchemy.exc.IntegrityError:
+    # Remeber to inluce 400 so that fronten knows it
+    return {"message": 'Email must be unique'}, 400
+  ######login
+@app.route('/users/login', methods=["POST"])
+def login():
+  user = models.User.query.filter_by(email=request.json["email"]).first()
+  if not user:
+    return {
+      'message': "User not found"
+    }, 404
+  # if user.password == request.json['password']:
+  #with encruption
+  if bcrypt.check_password_hash(user.password, request.json["password"]):
+    encrypted_id = jwt.encode({"user_id":user.id}, os.environ.get("JWT_SECRET"), algorithm="HS256")
+    return {
+      "user": user.to_json(),
+      "user_id": encrypted_id
+    }
+  else:
+    return {
+      "message": "Login failed"
+    }, 401
+      
+# app.route('/users/login', methods=["POST"])(login())
+    # try:
+    #     user = models.User.query.filter_by(email=request.json["email"]).first()
+    #     print(user)
+    #     if not user:
+    #         print('ok')
+    #         return {"message": "User not found"},401
+
+    #     # elif user.password == request.json["password"]:
+    #     elif bcrypt.check_password_hash(user.password, request.json["password"]):
+    #         encrypted_id = jwt.encode(
+    #             {"user_id": user.id}, os.environ.get("JWT_SECRET"), algorithm="HS256"
+    #         )
+    #         return {"user": user.to_json(), "User_id": encrypted_id}
+
+    #     else:
+    #         return {"message": "password incorrect"},402
+    # except Exception as e:
+    #   print(e)
+    #   return {"erorr": f"{e}"}
+
+# not working
+@app.route('/users/verify', methods=["GET"])
+def verify_user():
+  # print(request.headers) # => Authorusization: id
+  decrypted_id = jwt.decode(request.headers["Authorization"], os.environ.get("JWT_SECRET"), algorithms=["HS256"])["user_id"]
+  # user = models.User.query.filter_by(id=request.headers["Authorization"]).first() # How to look user up? We can't do it by request.json
+  # print(user)
+  user = models.User.query.filter_by(id=decrypted_id['id']).first()
+  # if not user:
+  #   return {
+  #     "message": "user not found"
+  #   }, 404
+  if user:
+    return {"user": user.to_json()}
+  else:
+    return {"message": "user not found"}, 404
+  # return {"user": user.to_json()}
+
+
+
 
 if __name__ == '__main__':
   port = os.environ.get('PORT') or 5000
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,48 +1,65 @@
-import { useState } from 'react'
-import axios from 'axios'
+import { useState } from "react";
+import axios from "axios";
 
 const Login = (props) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setError('')
-    axios.post(`${process.env.REACT_APP_BACKEND_URL}/users/login`, {
-      email, password
-    })
-    .then((response) => {
-      props.setUser(response.data.user)
-      localStorage.setItem('userId', response.data.user.id)
-    })
-    .catch((err) => {
-      setError(err.response.data.message)
-    })
-  }
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    const response = await axios.post(
+      // `${process.env.REACT_APP_BACKEND_URL}/users/login`,
+      "http://localhost:5000/users/login",
+      {
+        email,
+        password,
+      }
+    );
+
+    // .then((response) => {
+    props.setUser(response.data.user);
+    // localStorage.setItem("userId", response.data.user_id);
+    localStorage.setItem("userId", response.data.User_id);
+    // console.log(response);
+    // })
+    // .catch((err) => {
+    // setError(err.response.data.message);
+    // console.log(err);
+    // });
+  };
+
   return (
     <div>
       <h2>Log into your accout!</h2>
 
-      { error && 
-      <div className="error">{error}</div>}
-      
+      {error && <div className='error'>{error}</div>}
+
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="signup-email">Email:</label>
-          <input id="signup-email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <label htmlFor='signup-email'>Email:</label>
+          <input
+            id='signup-email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
-          <label htmlFor="signup-password">Password:</label>
-          <input id="signup-password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <label htmlFor='signup-password'>Password:</label>
+          <input
+            id='signup-password'
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
-          <input type="submit" value="Log in!" ></input>
+          <input type='submit' value='Log in!'></input>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
